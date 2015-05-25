2e603ba46e149b6181332c24a8a7f5c57514fc6b
Ana <pi@pepino-2.local>
2015-05-25 20:33:31 +0200

    Ejercicio módulo1

---
+++ mod1.js
@@ -0,0 +1,11 @@
+//Realizar un programa que saque por consola:
+//	(1) El numero pi con 8 decimales
+//var pi = Math.PI.toFixed(8)
+console.log("Pi con 8 decimales" + Math.PI.toFixed(8));
+
+//	(2) Dos lineas en blanco
+console.log();
+console.log();
+
+//	(3) Ristra de caracteres UNICODE -> \u55e8\uff0c\u4f60\u597d\u5417
+console.log("Ristra de caracteres UNICODE -> \\u55e8\\uff0c\\u4f60\\u597d\\u5417: \u55e8\uff0c\u4f60\u597d\u5417");
